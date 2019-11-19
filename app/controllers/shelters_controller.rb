b864f47dbc3aeef760dcec49de32255d08c60a86
msimon42 <msimon@uab.edu>
2019-11-19 15:30:49 -0700

    Add action for create shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -3,7 +3,24 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
+  def new
+
+  end
+
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+    shelter.save
+    redirect_to "/shelters"
+  end
+
 end
