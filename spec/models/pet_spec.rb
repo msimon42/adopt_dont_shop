c100f1510f68fa1b9e4202b891f5342a52e2f397
msimon42 <msimon@uab.edu>
2019-11-20 15:23:10 -0700

    Fix validation tests for pet

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Pet, type: :model do
   describe "validations" do
-    it {should validate_presence_of :image, :name, :approx_age, :sex, :shelter_id}
-  end   
+    it {should validate_presence_of :image}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :approx_age}
+    it {should validate_presence_of :sex}
+  end
 end
