09a86d8065747af97127a6c209d77b6f897a77b5
msimon42 <msimon@uab.edu>
2019-11-20 10:34:45 -0700

    Add test for relationships

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -4,4 +4,8 @@ describe Shelter, type: :model do
   describe "validations" do
     it {should validate_presence_of :name, :address, :city, :state, :zip}
   end
+
+  describe "relationships" do
+    it {should have_many :pets}
+  end
 end
