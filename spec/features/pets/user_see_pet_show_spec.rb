ed2971d6e0e0bc7e052b052ceeda7a50f3cec950
msimon42 <msimon@uab.edu>
2019-11-20 22:32:09 -0700

    Fix typo

--- spec/features/pets/user_see_pet_show_spec.rb
+++ spec/features/pets/user_see_pet_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'pet show page', type: :feature do
   it 'can see pet info' do
     pet_1 = Pet.create(
-      shelter_id: shelter_1.id,
+      shelter_id: 2,
       image: 'pet_1.jpg',
       name: 'Fido',
       description: 'Golden Retriver',
@@ -13,12 +13,13 @@ RSpec.describe 'pet show page', type: :feature do
 
       )
 
-    visit "pets/#{pet.id}"
+    visit "pets/#{pet_1.id}"
 
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_1.description)
     expect(page).to have_content(pet_1.approx_age)
     expect(page).to have_content(pet_1.sex)
     expect(page).to have_content(pet_1.adoption_status)
+  end
 
 end
