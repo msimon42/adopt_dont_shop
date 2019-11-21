ffa297bc6e80968c62491ecafb521efde39ea46e
msimon42 <msimon@uab.edu>
2019-11-21 15:24:13 -0700

    Add post for pets

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/shelters/:id/pets', to: 'shelters#pets'
   get '/pets/:id', to: 'pets#show'
   get '/shelters/:id/pets/new', to: 'pets#new'
+  post '/shelters/:id/pets', to: 'pets#create'
 end
