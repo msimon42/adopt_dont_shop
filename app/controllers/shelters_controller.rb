8981466b137ca88c542f6ad326eb46dd7d44cc23
msimon42 <msimon@uab.edu>
2019-11-21 15:21:40 -0700

    Turn shelter to instance variable

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -28,16 +28,16 @@ class SheltersController < ApplicationController
   end
 
   def update
-    shelter = Shelter.find(params[:id])
-    shelter.update({
+    @shelter = Shelter.find(params[:id])
+    @shelter.update({
       name: params[:name],
       address: params[:address],
       city: params[:city],
       state: params[:state],
       zip: params[:zip]
       })
-    shelter.save
-    redirect_to "/shelters/#{shelter.id}"
+    @shelter.save
+    redirect_to "/shelters/#{@shelter.id}"
   end
 
   def delete
