d4d6e7ce2326f91aeaf2ee664a5b9a6a60d243ae
msimon42 <msimon@uab.edu>
2019-11-20 21:54:59 -0700

    Add adoption status method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,9 +2,14 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   validates_presence_of :name, :approx_age, :description, :image, :adoptable?, :sex
 
-  def shelter_name(id)
-    shelter = Shelter.find(id)
+  def shelter_name
+    shelter = Shelter.find(self.shelter_id)
     shelter.name
-  end   
+  end
+
+  def adoption_status
+    return 'Available for adoption' if self.adoptable?
+    'Not available for adoption'
+  end
 
 end
