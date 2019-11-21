6acac6d5f76c819f5cbe4a9a86f31107a0a86455
msimon42 <msimon@uab.edu>
2019-11-20 21:24:50 -0700

    Fix broken activerecord query

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,8 @@
 class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
+  has_many :pets
 
-  has_many :pets 
+  def self.find_pets(shelter)
+    pets = Pet.where(shelter_id: shelter.id)
+  end
 end
