612e66d3af4593e2f16cf6229f2cc596862bfc03
msimon42 <msimon@uab.edu>
2019-11-26 16:55:28 -0600

    Fix validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
-  validates_presence_of :name, :approx_age, :description, :image, :adoptable?, :sex
+  validates_presence_of :name, :approx_age, :description, :image, :sex
 
   def shelter_name
     self.shelter.name
