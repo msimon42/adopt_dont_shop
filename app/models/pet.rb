4cc0c0c52fd7734a0e41d563d2f4c17798b8c310
msimon42 <msimon@uab.edu>
2019-11-26 13:43:19 -0600

    Refactor method to find shelter name

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,8 +3,7 @@ class Pet < ApplicationRecord
   validates_presence_of :name, :approx_age, :description, :image, :adoptable?, :sex
 
   def shelter_name
-    shelter = Shelter.find(self.shelter_id)
-    shelter.name
+    self.shelter.name
   end
 
   def adoption_status
