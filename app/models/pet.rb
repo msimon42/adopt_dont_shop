bc0f7c5da74146323e3e450c6eb5477aa6242640
msimon42 <msimon@uab.edu>
2019-11-26 21:03:00 -0600

    Add numericality validation for approx_age

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
   validates_presence_of :name, :approx_age, :description, :image, :sex
+  validates_numericality_of :approx_age
 
   def shelter_name
     self.shelter.name
