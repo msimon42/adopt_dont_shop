a2f8bf7188c0c2889ecbf3b7c5833c7bd2029610
msimon42 <msimon@uab.edu>
2019-11-26 18:02:54 -0600

    Improve test for edit shelter

--- spec/features/shelters/user_edit_form_spec.rb
+++ spec/features/shelters/user_edit_form_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'shelters edit page', type: :feature do
     )
 
     visit "/shelters/#{shelter_1.id}/edit"
-    
+
     expect(page).to have_content("Edit Shelter")
     expect(page).to have_field('Name')
     expect(page).to have_field('Address')
@@ -19,5 +19,11 @@ RSpec.describe 'shelters edit page', type: :feature do
     expect(page).to have_field('State')
     expect(page).to have_field('Zip')
 
+    fill_in 'Name', with: 'Shelter of Cat'
+
+    click_button 'Submit'
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    expect(page).to have_content("Shelter of Cat")
   end
 end
